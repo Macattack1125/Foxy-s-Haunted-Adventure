f15fbd9740fd930d5c07cb4c7835cce603794371
Macattack1125 <macraw50@kingshigh.school.nz>
2024-08-21 10:24:58 +1200

    starting to remake enemys

--- eScenes/enemy.gd
+++ eScenes/enemy.gd
@@ -1,16 +1,14 @@
 extends Area2D
 
-var direction = 1 
-
+var direction = 1
 func _ready():
 	pass 
 
 
-
 func _process(delta):
 	translate(Vector2.RIGHT * direction)
-	$AnimatedSprite2D.flip_h = direction < 0
+	
 
 
-func _on_timer_timeout():
-	direction *= -1
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
