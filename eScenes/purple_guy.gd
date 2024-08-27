4e7fc918442efc322039aa4cb730bdd72778c36d
Macattack1125 <macraw50@kingshigh.school.nz>
2024-08-28 09:31:31 +1200

    completed death effct for emeie

--- eScenes/purple_guy.gd
+++ eScenes/purple_guy.gd
@@ -4,7 +4,7 @@ var direction = 1
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
-
+const DEATH_EFFECT = preload("res://eScenes/death_effect.tscn")
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,6 +19,9 @@ func _physics_process(delta: float) -> void:
 	$AnimatedSprite2D.flip_h = direction > 0
 
 func die():
+	var new_explosion = DEATH_EFFECT.instantiate()
+	new_explosion.global_position = global_position
+	get_tree().current_scene.add_child(new_explosion)
 	GameManger.score +=50
 	queue_free()
 	
